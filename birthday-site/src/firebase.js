839c5caaad2e10b4f591e0b1ef8c6c920cc4228c
Karan Kumar <karanksxxx@gmail.com>
2025-10-26 14:04:03 +0530

    update

---
+++ birthday-site/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBmAs8PF-AaY9JC5qRjkqQmixiiIY6wKzg",
+  authDomain: "birthday-site-1436.firebaseapp.com",
+  projectId: "birthday-site-1436",
+  storageBucket: "birthday-site-1436.firebasestorage.app",
+  messagingSenderId: "1042506714101",
+  appId: "1:1042506714101:web:93ff09c70284022c076ebc"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
